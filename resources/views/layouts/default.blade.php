ec8a4ca26f57b04eb33e9917161e34352e17c29e
xuxi-hub <778022227@qq.com>
2017-03-16 08:09:39 +0000

    home help about _pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Oriidea - @yield('title', '')</title>
+
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:100">
+
+    </head>
+    <body>
+        @yield('content')
+    </body>
+</html>
